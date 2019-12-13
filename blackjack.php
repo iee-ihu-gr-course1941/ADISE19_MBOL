f8ec3a34d8c83e0bade9c4220f1df2a9bbb8d9d5
KyriakosOuzounis <57915478+KyriakosOuzounis@users.noreply.github.com>
2019-12-13 20:09:32 +0200

    Update blackjack.php

--- blackjack.php
+++ blackjack.php
@@ -3,8 +3,8 @@
 	require_once "lib/dbconnect.php";
 	require_once "lib/game.php";
 	
-	//$method=$_SERVER['REQUEST_METHOD'];
-	$method="PUT";
+	$method=$_SERVER['REQUEST_METHOD'];
+	
 	
 	$request=explode('/',trim($_SERVER['PATH_INFO'],'/'));
 	$input = json_decode(file_get_contents('php://input'),true);

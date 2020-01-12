8343be2e897358869d8ae627c6ce8d23f6701fd1
stavrosmavromatis <57561129+stavrosmavromatis@users.noreply.github.com>
2020-01-12 17:40:03 +0200

    ...

--- lib/dbconnect.php
+++ lib/dbconnect.php
@@ -1,10 +1,12 @@
 <?php
 	$host="localhost";
 	$db="blackjack";
-	$user="root";
-	$pass="";
+	require_once "db_upass.php";
+	
+	$user=$DB_USER;
+	$pass=$DB_PASS;
 	if(gethostbyname($host)=='users.iee.ihu.gr')
-		$mysqli=new mysqli($host,$user,$pass,$db,null,"/home/student/it/2017/it174896/mysql/run/mysql.sock");
+		$mysqli=new mysqli($host,$user,$pass,$db,null,'/home/student/it/2017/it174896/mysql/run/mysql.sock');
 	else
 		$mysqli=new mysqli($host,$user,$pass,$db);
 	
